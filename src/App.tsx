187e7a50dcc5e7d4eec5ecac508c16e6a4ee249f
unknown <chadbosch08@gmail.com>
2019-05-29 16:12:05 +0200

    Random push

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,74 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component {
+//   state:any;
+//   timerID:any;
+//   constructor(props:any) {
+//     super(props);
+//     this.state = {date: new Date()};
+//   }
+//   componentDidMount(){
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+//   componentWillUnmount(){
+//     clearInterval(this.timerID);
+//   }
+//   tick(){
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     ); 
+//   }
+// }
+
+class CheckBox extends React.Component {
+  state:any;
+	constructor(props:any) {
+		super(props);
+		this.state = {
+			checked: true
+		}	
+	}
+	
+	handleChecked = () => {
+		console.log('getting here');
+	this.setState({
+			checked: !this.state.checked
+		})
+	}
+	
+	render() {
+		let msg = "";
+		if(this.state.checked){
+			msg = 'checked'
+		} else {
+				msg = "unchecked"
+		}
+    return (
+        <div>
+					<input
+						type="checkbox"
+						onChange={this.handleChecked}
+						defaultChecked={this.state.checked}
+					/>
+				
+         	<h3>Checked is {msg}</h3>
+        </div>
+    )
+  }	
 }
 
-export default App;
+export default CheckBox;
+
